d780ad8930d6571fdace76089e76800fb26baf4c
Tohidul0 <tohidulalam967418@gmail.com>
2024-01-27 22:54:42 +0600

    mongdb connect ,env file add to hide pass

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,17 @@
+import { error } from 'console';
 import express  from  'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+// mongodb connect----------------------------
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("mongodb is connected")
+})
+.catch(error=>{
+    console.log(error);
+})
 
 const app = express();
 
--- README.md
+++ README.md
@@ -2,3 +2,7 @@ npm create vite
 install tailwind
 npm i react router dom
 npm i flowbite-react
+npm i nodemone
+npm i express
+npm i mongoose
+npm i dotenv
