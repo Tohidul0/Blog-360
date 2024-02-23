873f08301387793330faadae8aeb7ebf77beec08
Tohidul0 <tohidulalam967418@gmail.com>
2024-02-24 02:06:59 +0600

    orgin add for render deploy prblm solve

--- Api/index.js
+++ Api/index.js
@@ -30,7 +30,8 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin : "http://localhost:5173",
+        
+        origin : "https://blog-360.onrender.com",
         credentials : true,
     }  
 ));
